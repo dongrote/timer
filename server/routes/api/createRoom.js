cedb62d3b3918fba0285e9a6c2c45f46ded7cc96
Don Grote <don.grote@gmail.com>
2020-04-04 16:49:46 -0400

    Invalid call to core.Rooms.join
    Forgot in include the client id in the call to core.Rooms.join(clientId, roomId)

--- server/routes/api/createRoom.js
+++ server/routes/api/createRoom.js
@@ -1,9 +1,10 @@
 'use strict';
-const core = require('../../core');
+const _ = require('lodash'),
+  core = require('../../core');
 
 exports = module.exports = (req, res, next) => core.Rooms
   .create()
-  .then(room => core.Rooms.join(room)
+  .then(room => core.Rooms.join(_.get(req.cookies, 'io'), room)
     .then(() => core.Rooms.createTimerToken(room)))
   .then(jwt => res
     .cookie('jwt', jwt, {expires: core.jwt.expires(jwt)})
